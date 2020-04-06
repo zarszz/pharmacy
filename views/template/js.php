b9ccd98e56c73a39793d6c4f235dd383067f0677
mraehanakbar <43035458+mraehanakbar@users.noreply.github.com>
2020-04-06 20:15:13 +0700

    Add files via upload

---
+++ views/template/js.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/DataTables/dataTables.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/DataTables/js/jquery.dataTables.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/DataTables/js/dataTables.bootstrap4.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/sweetalert2/dist/sweetalert2.min.js'); ?>"></script>
